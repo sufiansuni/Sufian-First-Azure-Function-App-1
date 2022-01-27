deffb8433515bdb65e763b4faf3046d716fcd0dc
Sufian Suni <sufiansuni@gmail.com>
2022-01-27 14:06:41 +0800

    testing out azure key vault

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,6 +1,27 @@
 import azure.functions as func
+import os
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 def main(req: func.HttpRequest, commentsFile: str) -> str:
+    keyvault_name = os.environ["AZURE_KEYVAULT"]
+    keyvault_uri = f"https://{keyvault_name}.vault.azure.net/"
+
+    '''
+    Notes: 
+    - Set up of a Service Principal is required.
+    - Credentials need to be in invironment variables.
+
+    https://docs.microsoft.com/en-us/cli/azure/create-an-azure-service-principal-azure-cli
+    
+    '''
+    keyvault_credential = DefaultAzureCredential()
+
+    keyvault_secret_client = SecretClient(vault_url=keyvault_uri, credential=keyvault_credential)
+    sample_secret = keyvault_secret_client.get_secret("SampleKey").value
+
+    # print(sample_secret)
+    
     return func.HttpResponse(
         body = commentsFile, 
         headers = {
